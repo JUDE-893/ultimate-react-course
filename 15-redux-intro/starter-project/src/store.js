9661746377e4aeb95b5ad0abb423eb186ed1cb44
JUDE-893 <ichbinarif01@gmail.com>
2025-01-05 10:23:09 +0100

    update : re-create the redus store using the reduxjs-toolkit

--- 15-redux-intro/starter-project/src/store.js
+++ 15-redux-intro/starter-project/src/store.js
@@ -1,19 +1,10 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux';
-import {thunk} from 'redux-thunk';
+import {configureStore} from '@reduxjs/toolkit';
 import accountReducer from './features/account/accountReducerSlice';
 import customerReducer from './features/customer/customerReducerSlice';
 
-// sotre reducer router : combine all the reducer into a one centralized unit of reducers
-const reducerRouter = combineReducers({
-  account: accountReducer,
-  customer: customerReducer
-})
-
-const store = createStore(reducerRouter,applyMiddleware(thunk));
-
-
-//store.dispatch(depositeAction(1200))
-// store.dispatch(withdrawAction(400))
-// store.dispatch(requestLoanAction(1000,'to by a car for school'))
+//setting up the store
+const store = configureStore({
+  reducer: {account: accountReducer, customer: customerReducer}
+});
 export default store
 console.log(store.getState());
